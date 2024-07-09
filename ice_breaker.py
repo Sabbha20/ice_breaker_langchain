3ab1e85204d8c5a8241c1ac6bcd6c7c1b59c45ce
Sabbha20 <sabbha.power1@gmail.com>
2024-07-10 01:50:59 +0530

    importing dotenv and configuring it

--- ice_breaker.py
+++ ice_breaker.py
@@ -1,2 +1,13 @@
+import os
+from dotenv import load_dotenv
+
+from langchain.prompts.prompt import PromptTemplate
+from langchain_openai import ChatOpenAI
+
 if __name__ == "__main__":
+    load_dotenv()
     print("Hello Langchain")
+    # for k, v in os.environ.items():
+    #     print(f"{k}:\t {v}")
+    # print("\n\n")
+    print(os.environ["LS_COLORS"])
